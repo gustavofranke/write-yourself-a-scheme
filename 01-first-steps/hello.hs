eeafcb9a181aa0f50c93e00ef73882a46d70b75c
Gustavo Franke <gustavo@mindsandmachines.com>
2015-10-19 15:24:33 +0100

    minor modif to git ignore file

--- 01-first-steps/hello.hs
+++ 01-first-steps/hello.hs
@@ -6,6 +6,6 @@ main = do args <- getArgs
           putStrLn ("Hello, " ++ args !! 0)
 
 -- To compile and run the program, try something like this:
--- >> ghc −o hello_you hello.hs
+-- >> ghc -o hello_you hello.hs
 -- >> ./hello_you Gustavo
 -- Hello, Gustavo
