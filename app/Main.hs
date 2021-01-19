983aa021b5e6a3995853d509495c103d1ac0e4fa
Gustavo Franke <franke.gustavo@gmail.com>
2021-01-19 09:00:33 +0000

    add repl

--- app/Main.hs
+++ app/Main.hs
@@ -7,5 +7,8 @@ import Control.Monad (liftM)
 main :: IO ()
 main = do
     args   <- getArgs
-    evaled <- return $ liftM show $ readExpr (args !! 0) >>= eval
-    putStrLn $ extractValue $ trapError evaled
+    case length args of
+        0 -> runRepl
+        1 -> runOne $ args !! 0
+        otherwise -> putStrLn "Program takes only 0 or 1 argument"
+
--- src/WriteYourselfAScheme.hs
+++ src/WriteYourselfAScheme.hs
@@ -5,6 +5,7 @@ module WriteYourselfAScheme where
 import Control.Monad
 import Control.Monad.Error
 import Text.ParserCombinators.Parsec hiding (spaces)
+import System.IO hiding (try)
 
 -- |
 -- >>> parse symbol "" "$"
@@ -429,6 +430,28 @@ trapError action = catchError action (return . show)
 
 extractValue :: ThrowsError a -> a
 extractValue (Right val) = val
+
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr = return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+  result <- prompt
+  if pred result
+    then return ()
+    else action result >> until_ pred prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp>>> ") evalAndPrint
 
 -----------------------------------------------------------------------------
 -- Exercises
